067959a9f912848bb235bfcb4dc76b2a3568519d
Zch0 <3221458901@qq.com>
2025-10-04 10:03:07 +0800

    更新requirements.txt

--- src/generate_config.py
+++ src/generate_config.py
@@ -7,7 +7,7 @@ def generate_config():
     config["updateGameVersionTo"]=tomlkit.item("").comment("目标mod版本")
     config["modFolderFrom"]=tomlkit.item("").comment("源mod目录")
     config["modFolderTo"]=tomlkit.item("").comment("目标mod目录")
-    config["backupFolder"]=tomlkit.item("./backup").comment("备份目录")
+    config["backupFolder"]=tomlkit.item("").comment("备份目录")
     config["cacheFolder"]=tomlkit.item("./.cache").comment("缓存目录")
     config["updateSource"]=tomlkit.item("Modrinth").comment("更新来源")
     config["maxRetries"]=tomlkit.item(5).comment("最大重试次数")
